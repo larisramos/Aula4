77958a9f08f72c3dc7285b5a9f6cc4538238addc
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-03-03 12:02:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# print('Olá, Mundo!')
+# print(7+4)
+# print('7'+'4')
+# print('Olá',5)
+# nome = 'Larissa'
+# idade = 27
+# peso = 85
+# print(nome, idade, peso)
+nome=input('Qual o seu nome? \n')
+idade = input('Qual a sua idade? \n')
+peso = input ('Qual o seu peso? \n')
+print (nome, idade, peso)
